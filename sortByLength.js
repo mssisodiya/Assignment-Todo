9b6e21ce36e5a960084c9fa55051525bf596a7cd
mssisodiya <mssisodiya@bestpeers.com>
2020-12-03 18:55:08 +0530

    changed sortByLength

--- sortByLength.js
+++ sortByLength.js
@@ -4,8 +4,6 @@ sortByLength = str => {
   var f = j.sort(function (a, b) {
     return a.length - b.length;
   });
-  let e = f.toString()
-  let h = e.replace(/\,/g, " ");
-  var str2 = h.replace(/\,/g, " ");
-  return str2
+  let e = f.join(" ")
+  return e
 }
